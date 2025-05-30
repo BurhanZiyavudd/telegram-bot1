27d6d38022a86f8008a2cd1ae039fff69444bea5
Билол Юсупов <bilolusupov@Bilols-MacBook-Air.local>
2025-05-30 16:10:02 +0500

    new api added

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from aiogram.filters import Command
 
 from aiogram import Bot, Dispatcher, types
 from config import BOT_TOKEN
-
+import requests
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=BOT_TOKEN)
@@ -17,6 +17,13 @@ async def welcome(message: types.Message):
         f"Hello {message.from_user.first_name},\nIt's nice to see you!"
         f"\nHow may I help you today?"
     )
+
+@dp.message(Command('status'))
+async def status(msg: types.Message):
+    response = requests.get("https://shiotstandard-production.up.railway.app/devices/1/status")
+    await msg.answer(response.json())
+
+
 
 async def main():
     await dp.start_polling(bot)
